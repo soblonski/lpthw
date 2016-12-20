d888714922ad8acb4841893b4e8c2ca9aaca7056
soblonski <s.oblonski@outlook.de>
2016-12-20 22:06:59 +0100

    ex33.py

---
+++ ex33.py
@@ -0,0 +1,20 @@
+def dummy(n, incr):
+	# i = 0
+	numbers = []
+
+	# while i < n:
+		# print "At the top i is %d" % i
+		# numbers.append(i)
+		
+		# i += incr
+		# print "Numbers now: ", numbers 
+		# print "At the bottom i is %d" % i
+	 
+	 
+	# print "The numbers: "
+	 
+	# for num in numbers:
+		# print num
+	for i in range(0, n, incr):
+		numbers.append(i)
+	print numbers
